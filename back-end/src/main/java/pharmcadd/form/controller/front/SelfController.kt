b23dc164533b71e3a45f30d01bad2fe9f4fac13f
leecheongsu <leecheongsu@pharmcadd.com>
2022-03-22 12:00:04 +0900

    :zap: 유저 답변 취소 요청 리스트 API 추가

--- back-end/src/main/java/pharmcadd/form/controller/front/SelfController.kt
+++ back-end/src/main/java/pharmcadd/form/controller/front/SelfController.kt
@@ -6,9 +6,18 @@ import org.springframework.web.bind.annotation.*
 import pharmcadd.form.common.controller.BaseController
 import pharmcadd.form.common.exception.BadRequest
 import pharmcadd.form.common.exception.NotFound
+import pharmcadd.form.common.extension.plus
+import pharmcadd.form.common.extension.withLastTime
+import pharmcadd.form.common.extension.withZeroTime
+import pharmcadd.form.common.extension.zoneOffset
+import pharmcadd.form.common.util.pagination.DataTablePagination
+import pharmcadd.form.controller.front.form.AnswerCancelListForm
 import pharmcadd.form.controller.front.form.ChangePasswordForm
 import pharmcadd.form.controller.front.form.JoinForm
+import pharmcadd.form.jooq.Tables.CANCEL_ANSWER
+import pharmcadd.form.jooq.tables.pojos.CancelAnswer
 import pharmcadd.form.model.UserDetail
+import pharmcadd.form.service.TimeZoneService
 import pharmcadd.form.service.UserService
 
 @RestController
@@ -20,6 +29,9 @@ class SelfController : BaseController() {
 
     @Autowired
     lateinit var passwordEncoder: PasswordEncoder
+
+    @Autowired
+    lateinit var timeZoneService: TimeZoneService
 
     @GetMapping
     fun detail(): UserDetail {
@@ -49,5 +61,47 @@ class SelfController : BaseController() {
     @DeleteMapping("/positions/{positionId}")
     fun deletePosition(@PathVariable("positionId") positionId: Long) {
         userService.deletePosition(securityService.userId, positionId)
+    }
+
+    @GetMapping("/answer-cancels")
+    fun answerCancels(form: AnswerCancelListForm): DataTablePagination<CancelAnswer> {
+        val userId = securityService.userId
+
+        val query = dsl
+            .selectFrom(
+                CANCEL_ANSWER
+            )
+            .where(
+                CANCEL_ANSWER.USER_ID.eq(userId)
+                    .and(CANCEL_ANSWER.DELETED_AT.isNull)
+            )
+
+        if(form.status != null) {
+            query.and(
+                CANCEL_ANSWER.STATUS.eq(form.status)
+            )
+        }
+
+        val rangeFrom = form.rangeFrom
+        val rangeTo = form.rangeTo
+        if (rangeFrom != null || rangeTo != null) {
+            val timeZone = timeZoneService.findByUserId(userId)!!
+            val zoneOffset = timeZone.zoneOffset()
+            if(rangeFrom != null) {
+                query.and(
+                    CANCEL_ANSWER.CREATED_AT.greaterOrEqual(rangeFrom.withZeroTime()+zoneOffset)
+                )
+            }
+
+            if(rangeTo != null) {
+                query.and(
+                    CANCEL_ANSWER.CREATED_AT.lessOrEqual(rangeTo.withLastTime() + zoneOffset)
+                )
+            }
+        }
+
+       return DataTablePagination.of(dsl, query, form) {
+           it.into(CancelAnswer::class.java)
+       }
     }
 }
