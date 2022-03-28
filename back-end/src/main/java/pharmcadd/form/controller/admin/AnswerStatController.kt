fade3c2b006b25d22e6ae536ffa4ebff2a0cd6b8
오지혜 <ojhwlxk@pharmcadd.com>
2022-03-28 15:09:16 +0900

    ✨ answerStats - 답변 통계 추가

--- back-end/src/main/java/pharmcadd/form/controller/admin/AnswerStatController.kt
+++ back-end/src/main/java/pharmcadd/form/controller/admin/AnswerStatController.kt
@@ -1,13 +1,15 @@
 package pharmcadd.form.controller.admin
 
+import org.jooq.impl.DSL
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import pharmcadd.form.common.controller.BaseController
 import pharmcadd.form.common.util.pagination.DataTablePagination
 import pharmcadd.form.controller.admin.form.AnswerStatListForm
-import pharmcadd.form.jooq.Tables.ANSWER_STAT
-import pharmcadd.form.jooq.Tables.CAMPAIGN
+import pharmcadd.form.controller.admin.form.AnswerStatsForm
+import pharmcadd.form.jooq.Tables.*
 import pharmcadd.form.jooq.tables.pojos.AnswerStat
 
 @RestController
@@ -36,5 +38,41 @@ class AnswerStatController : BaseController() {
         return DataTablePagination.of(dsl, query, form) {
             it.into(AnswerStat::class.java)
         }
+    }
+
+    @GetMapping("/{campaign_id}")
+    fun stats(@PathVariable("campaign_id") campaign_id: Long): List<AnswerStatsForm> {
+        val answersMap = dsl
+            .select(
+                ANSWER.QUESTION_ID.`as`("question_id"),
+                ANSWER.OPTION_ID.`as`("option_id"),
+                ANSWER.TEXT.`as`("text"),
+                DSL.count(ANSWER.OPTION_ID).`as`("count"),
+            )
+            .from(ANSWER)
+            .where(
+                ANSWER.CAMPAIGN_ID.eq(campaign_id)
+                    .and(ANSWER.OPTION_ID.isNotNull)
+                    .and(ANSWER.DELETED_AT.isNull)
+            )
+            .groupBy(ANSWER.QUESTION_ID, ANSWER.OPTION_ID, ANSWER.TEXT)
+            .fetch()
+            .groupBy { it.get("question_id", Long::class.java) }
+
+        return answersMap
+            .toList()
+            .map { (question_id, answers) ->
+                AnswerStatsForm(
+                    questionId = question_id,
+                    total = answers.fold(0) { total, r -> total + r.get("count", Int::class.java) },
+                    optionStats = answers.map { a ->
+                        AnswerStatsForm.OptionStatsForm(
+                            optionId = a.get("option_id", Long::class.java),
+                            text = a.get("text", String::class.java),
+                            count = a.get("count", Int::class.java),
+                        )
+                    }
+                )
+            }
     }
 }
--- back-end/src/main/java/pharmcadd/form/controller/admin/form/AnswerStatListForm.kt
+++ back-end/src/main/java/pharmcadd/form/controller/admin/form/AnswerStatListForm.kt
@@ -6,3 +6,15 @@ class AnswerStatListForm : DataTableForm() {
     var formId: Long? = null
     var campaignId: Long? = null
 }
+
+data class AnswerStatsForm(
+    val questionId: Long,
+    val total: Int,
+    val optionStats: List<OptionStatsForm> = emptyList(),
+) {
+    data class OptionStatsForm(
+        val optionId: Long,
+        val text: String,
+        val count: Int,
+    )
+}
