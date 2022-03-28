4100abd7938289afac8a07698d5643093137896f
leecheongsu <leecheongsu@pharmcadd.com>
2022-03-28 09:43:01 +0900

    :lipstick: bottom-menu 그림자 처리

--- front-end/assets/tableHeaders.js
+++ front-end/assets/tableHeaders.js
@@ -3,26 +3,26 @@ export const FORMS_HEADER = [
     { label: '제목', value: 'title', className: 'w-full' },
     { label: '생성자', value: 'createdByName' },
     { label: '수정자', value: 'updatedByName' },
-    { label: '생성일', value: 'createdAt', dayFormat: true },
-    { label: '수정일', value: 'updatedAt', dayFormat: true },
+    { label: '생성일', value: 'createdAt', dayFormat: true, sortable: true },
+    { label: '수정일', value: 'updatedAt', dayFormat: true, sortable: true },
     { label: '', value: 'description' },
 ]
 
 export const CAMPAIGNS_HEADER = [
     { label: 'No.', value: 'id', sortable : true },
-    { label: '제목', value: 'title', className: 'w-full' },
+    { label: '제목', value: 'title', className: 'w-full', sortable: true },
     { label: '상태', value: 'status' },
     { label: '기간', value: 'startsAt' },
-    { label: '생성일', value: 'createdAt', dayFormat: true },
+    { label: '생성일', value: 'createdAt', dayFormat: true, sortable: true },
 ]
 
 export const USERS_HEADER = [
     { label: 'No.', value: 'id', sortable: true },
-    { label: '이름', value: 'name', className: 'w-1/4'},
-    { label: '메일', value: 'email' },
+    { label: '이름', value: 'name', className: 'w-1/4', sortable: true},
+    { label: '메일', value: 'email', sortable: true },
     { label: '부서', value: 'groupNames' },
     { label: '직책', value: 'positionNames' },
-    { label: '생성일', value: 'createdAt', dayFormat: true },
+    { label: '생성일', value: 'createdAt', dayFormat: true, sortable: true },
     { label: '활성화', value: 'active' },
 ]
 
